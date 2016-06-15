40a7285762d993117fa990cb79596ff018b78f03
saruman9 <rum.274.4@gmail.com>
2016-06-15 16:30:54 +0300

    Renamed examples, added fn get list languages, corrected name of languages, etc.

--- understand-sys/examples/demo.rs
+++ understand-sys/examples/demo.rs
@@ -3,30 +3,47 @@ extern crate understand_sys;
 use std::ffi::{CStr, CString};
 use std::os::raw::{c_char};
 use std::env::args;
+use std::mem;
 
 use understand_sys::*;
 
 fn main() {
     let program_args: Vec<String> = args().collect();
     if program_args.len() != 2 {
-        panic!("Expected one argument.");
+        println!("Expected one argument - path to UDB database.");
+        std::process::exit(2);
     }
 
-    let num_build: &str = unsafe { CStr::from_ptr(udbInfoBuild()).to_str().unwrap() };
-    println!("num_build: {}", num_build);
+    // Get number of build
+    unsafe{
+        let num_build: &str = CStr::from_ptr(udbInfoBuild()).to_str().unwrap();
+        println!("num_build: {}", num_build);
+    }
 
-    let udb_path_str: *mut c_char = CString::new(&program_args[1][..]).unwrap().into_raw();
-    let udb_status: UdbStatus = unsafe { udbDbOpen(udb_path_str) };
-    println!("udb_status: {:?}", udb_status);
+    unsafe {
+        // Open the UDB database
+        let udb_path_str: *mut c_char = CString::new(&program_args[1][..]).unwrap().into_raw();
+        let udb_status: UdbStatus = udbDbOpen(udb_path_str);
+        println!("udb_status: {:?}", udb_status);
 
-    let udb_status_text: &CStr = unsafe { CStr::from_ptr(udbStatusText(udb_status))};
-    println!("udb_status_text: {}", udb_status_text.to_string_lossy());
+        // Get status of UDB database
+        let udb_status_text: &CStr = CStr::from_ptr(udbStatusText(udb_status));
+        println!("udb_status_text: {}", udb_status_text.to_string_lossy());
+    }
 
-    let udb_languages: UdbLanguage = unsafe { udbDbLanguage() };
-    let udb_languages_vec: Vec<&'static str> = get_language_from_bitmask(udb_languages);
-    println!("udb_languages:");
-    for language in udb_languages_vec {
-        println!("\t{}", language);
+    unsafe{
+        // Get bitmask of languages in UDB database
+        let udb_languages: UdbLanguage = udbDbLanguage();
+        let udb_languages_vec: Vec<&'static str> = get_language_from_bitmask(udb_languages);
+        println!("udb_languages:");
+        for language in udb_languages_vec {
+            println!("\t{}", language);
+        }
+
+        // Get first string from ptr to structure of languages
+        let udb_language_strings = udbLanguageStrings(udb_languages);
+        let lang: &CStr = CStr::from_ptr(*udb_language_strings.offset(0));
+        println!("{:?}", lang);
     }
 
     let udb_language_strings = unsafe { udbLanguageStrings(udb_languages) };
@@ -63,10 +80,10 @@ fn get_language_from_bitmask(language: UdbLanguage) -> Vec<&'static str> {
         vec_of_languages.push("C#");
     }
     if (language & (UdbLanguage_::Udb_language_Cobol as u16)) != 0 {
-        vec_of_languages.push("Cobol");
+        vec_of_languages.push("COBOL");
     }
     if (language & (UdbLanguage_::Udb_language_Fortran as u16)) != 0 {
-        vec_of_languages.push("Fortran");
+        vec_of_languages.push("FORTRAN");
     }
     if (language & (UdbLanguage_::Udb_language_Java as u16)) != 0 {
         vec_of_languages.push("Java");
@@ -78,7 +95,7 @@ fn get_language_from_bitmask(language: UdbLanguage) -> Vec<&'static str> {
         vec_of_languages.push("Pascal");
     }
     if (language & (UdbLanguage_::Udb_language_Plm as u16)) != 0 {
-        vec_of_languages.push("Plm");
+        vec_of_languages.push("PL/M");
     }
     if (language & (UdbLanguage_::Udb_language_Python as u16)) != 0 {
         vec_of_languages.push("Python");
